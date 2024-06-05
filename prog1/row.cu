d0f2f883ae9fc5dee7c1bf0f4474a79499aba8b6
Gui <guicostaantunes@gmail.com>
2024-06-05 23:32:26 +0100

    added row doc

--- prog1/row.cu
+++ prog1/row.cu
@@ -28,15 +28,23 @@ static dim3 getBestGridSize(int iteration);
 static dim3 getBestBlockSize(int iteration);
 static double get_delta_time(void);
 
+/**
+ * \brief Main function.
+ *
+ * \param argc number of arguments
+ * \param argv arguments
+ *
+ * \return exit status
+ */
 int main(int argc, char **argv)
 {
     /* set up the device */
     int dev = 0;
 
-    cudaDeviceProp deviceProp;
-    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
-    printf("Using Device %d: %s\n", dev, deviceProp.name);
-    CHECK(cudaSetDevice(dev));
+    cudaDeviceProp deviceProp; /* to store device properties */
+    CHECK(cudaGetDeviceProperties(&deviceProp, dev)); /* get device properties */
+    printf("Using Device %d: %s\n", dev, deviceProp.name); /* print device name */
+    CHECK(cudaSetDevice(dev)); /* set the device */
 
     /* parse command line */
     if (argc < 5)
@@ -45,12 +53,12 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    char *fileName = NULL;
-    int opt, sortType = 0;
-    int K;
-    int rowsize = 1024;
-    int colsize = 1024;
-    int matrixSize = rowsize * colsize;
+    char *fileName = NULL; /* file name */
+    int opt, sortType = 0; /* opt, sort type */
+    int K; /* number of subsequences */
+    int rowsize = 1024; /* row size */
+    int colsize = 1024; /* column size */
+    int matrixSize = rowsize * colsize; /* matrix size */
 
     while ((opt = getopt(argc, argv, "k:s:f:h")) != -1)
     {
@@ -76,61 +84,72 @@ int main(int argc, char **argv)
 
     /* read file */
     int fileSize;
-    FILE *file = fopen(fileName, "rb");
+    FILE *file = fopen(fileName, "rb"); /* open file */
 
-    if (file == NULL)
+    if (file == NULL) /* check if file is null */
     {
         printf("Error: Could not open file %s\n", fileName);
         exit(EXIT_FAILURE);
     }
-    if (fread(&fileSize, sizeof(int), 1, file) != 1)
+    if (fread(&fileSize, sizeof(int), 1, file) != 1) /* read file size */
     {
         printf("Error: Could not read file size\n");
         exit(EXIT_FAILURE);
     }
-    int *data = (int *)malloc(matrixSize * sizeof(int));
-    if (data == NULL)
+    int *data = (int *)malloc(matrixSize * sizeof(int)); /* allocate memory for data */
+    if (data == NULL) /* check if data is null */
     {
         printf("Error: Could not allocate memory\n");
         exit(EXIT_FAILURE);
     }
-    if (fread(data, sizeof(int), fileSize, file) != fileSize)
+    if (fread(data, sizeof(int), fileSize, file) != fileSize) /* read file data */
     {
         printf("Error: Could not read file data\n");
         exit(EXIT_FAILURE);
     }
-    fclose(file);
+    fclose(file); /* close file */
 
     /* reserve memory for the gpu */
     int *d_data;
-    CHECK(cudaMalloc((void **)&d_data, matrixSize * sizeof(int)));
-    CHECK(cudaMemcpy(d_data, data, matrixSize * sizeof(int), cudaMemcpyHostToDevice));
+    CHECK(cudaMalloc((void **)&d_data, matrixSize * sizeof(int))); /* allocate memory for d_data */
+    CHECK(cudaMemcpy(d_data, data, matrixSize * sizeof(int), cudaMemcpyHostToDevice)); /* copy data to d_data */
 
-    int numMerges = log2(K);
+    int numMerges = log2(K); /* number of merges */
 
-    dim3 gridSize = getBestGridSize(numMerges);
-    dim3 blockSize = getBestBlockSize(numMerges);
+    dim3 gridSize = getBestGridSize(numMerges); /* get best grid size */
+    dim3 blockSize = getBestBlockSize(numMerges); /* get best block size */
 
     (void)get_delta_time();
 
-    bitonicSort<<<gridSize, blockSize>>>(d_data, sortType, matrixSize, K, rowsize);
-    CHECK(cudaDeviceSynchronize());
-    CHECK(cudaGetLastError());
+    bitonicSort<<<gridSize, blockSize>>>(d_data, sortType, matrixSize, K, rowsize); /* bitonic sort */
+    CHECK(cudaDeviceSynchronize()); /* synchronize device */
+    CHECK(cudaGetLastError());  /* check for errors */
 
-    double dt = get_delta_time();
-    printf("GPU time: %f s\n", dt);
+    double dt = get_delta_time(); /* get delta time */
+    printf("GPU time: %f s\n", dt); /* print time */
 
-    validateArray<<<dim3(1, 1, 1), dim3(1, 1, 1)>>>(d_data, sortType, matrixSize, rowsize);
-    CHECK(cudaDeviceSynchronize());
-    CHECK(cudaGetLastError());
+    validateArray<<<dim3(1, 1, 1), dim3(1, 1, 1)>>>(d_data, sortType, matrixSize, rowsize); /* validate array */
+    CHECK(cudaDeviceSynchronize()); /* synchronize device */
+    CHECK(cudaGetLastError()); /* check for errors */
 
-    free(data);
-    CHECK(cudaFree(d_data));
-    CHECK(cudaDeviceReset());
+    free(data); /* free data */
+    CHECK(cudaFree(d_data)); /* free d_data */
+    CHECK(cudaDeviceReset()); /* reset device */
 
     return EXIT_SUCCESS;
 }
 
+/**
+ * \brief Function bitonicSort.
+ *
+ * Its role is to sort an integer array using the bitonic sort algorithm.
+ *
+ * \param arr array
+ * \param sortType sort type
+ * \param N number of elements
+ * \param K number of subsequences
+ * \param rowsize row size
+ */
 __global__ static void bitonicSort(int *arr, int sortType, int N, int K, int rowsize)
 {
     int x = threadIdx.x + blockDim.x * blockIdx.x;
@@ -172,6 +191,17 @@ __global__ static void bitonicSort(int *arr, int sortType, int N, int K, int row
     }
 }
 
+/**
+ * \brief Function sort.
+ *
+ * Its role is to sort an integer array.
+ *
+ * \param arr array
+ * \param start start index
+ * \param sortType sort type
+ * \param N number of elements
+ * \param K row size
+ */
 __device__ static void sort(int *arr, int start, int sortType, int N, int K)
 {   
     for (int i = 2; i <= N; i <<= 1)
@@ -182,6 +212,17 @@ __device__ static void sort(int *arr, int start, int sortType, int N, int K)
     }
 }
 
+/**
+ * \brief Function merge.
+ *
+ * Its role is to merge two subsequences.
+ *
+ * \param arr array
+ * \param start start index
+ * \param sortType sort type
+ * \param N number of elements
+ * \param K row size
+ */
 __device__ static void merge(int *arr, int start, int sortType, int N, int K)
 {
     for (int j = N >> 1; j > 0; j >>= 1)
@@ -189,7 +230,7 @@ __device__ static void merge(int *arr, int start, int sortType, int N, int K)
         for (int k = 0; k < N; k += (j << 1))
         {
             for (int l = k; l < j + k; l++)
-            {
+            { 
                 if (arr[start + l] > arr[start + l + j] ^ sortType)
                 {
                     int temp = arr[start + l];
@@ -206,9 +247,10 @@ __device__ static void merge(int *arr, int start, int sortType, int N, int K)
  *
  *  Its role is to validate an integer array.
  *
- *  \param array pointer to the array
- *  \param size array size
- *  \param sortType sort type
+ * \param array array to validate
+ * \param sortType sort type
+ * \param N number of elements
+ * \param K row size
  */
 __global__ static void validateArray(int *array, int sortType, int N, int K)
 {
@@ -234,6 +276,9 @@ __global__ static void validateArray(int *array, int sortType, int N, int K)
     }
 };
 
+/**
+ * \brief Grid options.
+ */
 static dim3 gridOptions[11] = {
     dim3(1 << 0, 1 << 0, 1 << 0),
     dim3(1 << 0, 1 << 0, 1 << 0),
@@ -248,6 +293,9 @@ static dim3 gridOptions[11] = {
     dim3(1 << 0, 1 << 0, 1 << 0),
 };
 
+/**
+ * \brief Block options.
+ */
 static dim3 blockOptions[11] = {
     dim3(1 << 0, 1 << 0, 1 << 0),
     dim3(1 << 1, 1 << 0, 1 << 0),
@@ -262,16 +310,35 @@ static dim3 blockOptions[11] = {
     dim3(1 << 10, 1 << 0, 1 << 0),
 };
 
+/**
+ * \brief Get the best grid size for the given iteration.
+ *
+ * \param iteration iteration
+ *
+ * \return best grid size
+ */
 static dim3 getBestGridSize(int iteration)
 {
     return gridOptions[iteration];
 };
 
+/**
+ * \brief Get the best block size for the given iteration.
+ *
+ * \param iteration iteration
+ *
+ * \return best block size
+ */
 static dim3 getBestBlockSize(int iteration)
 {
     return blockOptions[iteration];
 };
 
+/**
+ * \brief Get the process time that has elapsed since last call of this time.
+ *
+ * \return process elapsed time
+ */
 static double get_delta_time(void)
 {
     static struct timespec t0, t1;
